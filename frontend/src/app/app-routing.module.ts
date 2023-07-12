f3f2c81641857fe8f938271e35bb17d6a89e2203
igorpadua <igorpadua@tutanota.com>
2023-07-12 17:02:04 -0300

    feat: iniciado a tela principal

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {WriteComponent} from "./components/write/write.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'write',
+    pathMatch: 'full'
+  },
+  {
+    path: 'write',
+    component: WriteComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WriteComponent } from './components/write/write.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WriteComponent
   ],
   imports: [
     BrowserModule,
