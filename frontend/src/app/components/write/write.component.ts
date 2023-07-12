27697f398ab0367de7674ee4a3450c35e97238fc
igorpadua <igorpadua@tutanota.com>
2023-07-12 18:27:40 -0300

    feat: atualiza o current_position quando o usuário modifica o cursor do mouse

--- frontend/src/app/components/write/write.component.ts
+++ frontend/src/app/components/write/write.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {DocumentService} from "../../service/document.service";
 import {Document} from "../../model/document.model";
+import {EditingSession} from "../../model/editing-session.model";
+import {EditingSessionService} from "../../service/editing-session.service";
 
 @Component({
   selector: 'app-write',
@@ -15,7 +17,14 @@ export class WriteComponent implements OnInit {
     content: ''
   }
 
-  constructor(private documentService: DocumentService) {}
+  editingSession: EditingSession = {
+    current_position: 0,
+    document_id: 0,
+    is_editing_active: false,
+    user_id: 0
+  }
+
+  constructor(private documentService: DocumentService, private editingSessionService: EditingSessionService) {}
 
   ngOnInit(): void {
     this.documentService.getDocuments().subscribe(
@@ -23,7 +32,20 @@ export class WriteComponent implements OnInit {
         this.document = data[0];
       }
     )
+
+    this.editingSessionService.getEditingSessions().subscribe(
+      (data: EditingSession[]) => {
+        this.editingSession = data[0];
+        console.log(this.editingSession);
+      }
+    )
   }
 
+  atualizaPosicaoCorrent(event: MouseEvent) {
+    const textarea = event.target as HTMLTextAreaElement;
+    const posicao = textarea.selectionStart;
 
+    this.editingSession.current_position = posicao;
+    this.editingSessionService.putEditingSession(this.editingSession).subscribe();
+  }
 }
