9b9b55f7356fdc006a612ec894effb9312a1559b
igorpadua <igorpadua@tutanota.com>
2023-07-12 18:26:56 -0300

    feat: service e model do editing session

---
+++ frontend/src/app/model/editing-session.model.ts
@@ -0,0 +1,6 @@
+export interface EditingSession {
+  current_position: number
+  document_id: number
+  is_editing_active: boolean
+  user_id: number
+}
---
+++ frontend/src/app/service/editing-session.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {EditingSession} from "../model/editing-session.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EditingSessionService {
+
+  urlBase = 'http://localhost:8080/editing-sessions';
+  constructor(private httpClient: HttpClient) { }
+
+  getEditingSessions(): Observable<EditingSession[]> {
+    return this.httpClient.get<EditingSession[]>(this.urlBase);
+  }
+
+  putEditingSession(editingSession: EditingSession): Observable<EditingSession> {
+    const url = this.urlBase + '/' + editingSession.user_id + '/' + editingSession.document_id;
+    return this.httpClient.put<EditingSession>(url, editingSession);
+  }
+}
